51dffde3a92a970306dd3018c61779170e46c334
jgart <jgart@dismail.de>
2023-06-26 23:42:18 -0500

    Completed variables section.

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -3,10 +3,10 @@
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+//  static section of a binary where static variables are held
 
 fn main() {
-    let x;
+    let x: i32 = 10;
     if x == 10 {
         println!("x is ten!");
     } else {

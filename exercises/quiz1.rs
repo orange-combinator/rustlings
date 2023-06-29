c8b819ab838fb8fbca2052464ac89740eff403b1
jgart <jgart@dismail.de>
2023-06-28 22:24:45 -0500

    Add quiz 1.

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x: i64) {
+    if x > 40 {
+        x * 2
+    } else {
+        x
+    }
+}
 
 // Don't modify this function!
 #[test]

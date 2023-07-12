21c6a54d2c0c7513f339a4be4e42f91e8d393d87
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-07-11 22:50:30 -0700

    primitive_types3 - arrays
    - using, `let a = [val; qty];` initialization format

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,14 +5,19 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    // let a = ???
+    let a = [42; 101];
+
+    println!("Created `a` using `let a = [42; 101];`");
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
     }
+
+    println!("The length of a = {}", a.len());
+    println!("a[0] = {}", a[0]);
 }

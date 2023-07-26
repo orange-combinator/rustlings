af707f4e3979308f36dbe363a40295be76094543
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-07-25 20:53:29 -0700

    errors2 - Result, match, ParseIntError

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -28,7 +28,10 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    match qty {
+      Ok(qty2) => Ok(qty2 * cost_per_item + processing_fee),
+      Err(pie) => Err(pie),
+    }
 }
 
 #[cfg(test)]

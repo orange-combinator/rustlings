600727de54e00261690f2262fb03cf14128f4ae0
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-07-28 23:32:31 -0700

    quiz3
    - generics
    - `std::fmt::Display`

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -18,14 +18,14 @@
 
 // I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T, // f32,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
+impl<T: std::fmt::Display> ReportCard<T> {
+    fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
     }
@@ -52,7 +52,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+", // 2.1,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };

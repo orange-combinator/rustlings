d99d69057fbeefbb68601fd68d4440772c3bbfda
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-08-01 01:17:38 -0700

    iterators4 - factorial
    - My BIG mistake was putting the range inside [ ]'s
    - solution: `let nums = 1..=nums;`
    - then, `.into_iter().fold(...)`
    - `.fold(init val, |acc, x| computation)`
    - x is brought in from an `.iter()`
    - computation will be sent back in as acc

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +14,15 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    let nums = 1..=num;
+    let fac: u64 = nums.into_iter().fold(1, |acc, x| acc * x);
+    fac
+}
+
+fn main() {
+    // Your code goes here
+    println!("Hello, world!");
 }
 
 #[cfg(test)]
